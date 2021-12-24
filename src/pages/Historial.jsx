d62e1d7de1940bd54a1fdce241b58add6e7d67c9
Carlos Rojas <caranroja@outlook.com>
2021-12-23 23:36:19 -0500

    Se crea tabla para mostrar historial de jugadores

--- src/pages/Historial.jsx
+++ src/pages/Historial.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Historial = () => {
     return (
-        <div>
-            soy el historial
+        <div className='flex flex-col h-screen items-center'>
+            <h1 className='font-extrabold text-5xl text-center mt-7'>Historial de jugadores</h1>
+            <table className='mt-8 text-xl border border-black'>
+                <thead>
+                    <tr>
+                        <th className='px-16'>Nombre</th>
+                        <th className='px-16'>Puntaje</th>
+                        <th className='px-16'>Fecha</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='px-16 text-center'>Carlos</td>
+                        <td className='px-16 text-center'>100</td>
+                        <td className='px-16 text-center'>12-03-04, 05:06 p.m.</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
