970f8658fb6d14015a06d1ab97beed0ec1bcaf73
Carlos Rojas <caranroja@outlook.com>
2021-12-23 20:22:14 -0500

    Se agrega teilwind para dar estilos a la pagina

---
+++ src/pages/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div className='font-extrabold rounded-xl'>
+            Hola mundo
+        </div>
+    )
+}
+
+export default Index
